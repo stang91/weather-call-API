06c54c3b81c8ea66a49ca7d19578470a00f4eefc
stang91 <s.tang32191@gmail.com>
2022-08-07 20:10:55 -0700

    base on files

--- assets/script.js
+++ assets/script.js
@@ -13,20 +13,20 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-let lat;
-let lon;
-let exclude;
-let city=$('#city').val();
-let state=$('#state-Code').val();
 let keyAPI='cda125b32a94084e8ae8c5922b57599d';
-let requestWeatherURL='http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+keyAPI+'&units=imperial';
 let submitBtn=$('#submit');
 
 function createCurrentForecast(){
-    
+    let weatherCurrentDiscription=JSON.parse(localStorage.getItem(city)).weather[0].description;
+    let weatherCurrentIcon=JSON.parse(localStorage.getItem(city)).weather[0].icon;
+    let weatherCurrentMain=JSON.parse(localStorage.getItem(city)).main;//object
+    let weatherCurrentSunrise=JSON.parse(localStorage.getItem(city)).sys.sunrise;
+    let weatherCurrentSunset=JSON.parse(localStorage.getItem(city)).sys.sunset;
+
 }
 
 function createFutureForecast(){
+//use nth child 8n+4 to get noon weather forecast
 
 }
 
@@ -34,29 +34,46 @@ function createPreviousSearchBtn(){
 
 }
 
+
+
 submitBtn.click(function(){ 
     let city=$('#city').val();
     let state=$('#state-Code').val();
+    let keyAPI='cda125b32a94084e8ae8c5922b57599d';
     let requestLonLatURL='http://api.openweathermap.org/geo/1.0/direct?q='+city+','+state+',US&appid='+keyAPI;
     fetch(requestLonLatURL, {
         method: 'GET' //GET is the default.
-        })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            lon=data.lon;
-            lat=data.lat;
-        });
-    // fetch(requestWeatherURL, {
-    //     method: 'GET', //GET is the default.
-    //     })
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (data1) {
-    //         console.log(data1);
-    //         localStorage.setItem(city,JSON.stringify(data));
-    //     });
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // console.log(data);
+        localStorage.setItem(city,JSON.stringify(data));
+    });
+    let lon=JSON.parse(localStorage.getItem(city))[0].lon;
+    let lat=JSON.parse(localStorage.getItem(city))[0].lat;
+   
+    let requestCurrentWeatherURL='http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+keyAPI+'&units=imperial';
+    fetch(requestCurrentWeatherURL, {
+        method: 'GET', //GET is the default.
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data1) {
+        console.log(data1);
+        localStorage.setItem(city+'1',JSON.stringify(data1));
+    });
+
+    let requestForecastWeatherURL='http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+keyAPI+'&units=imperial';
+    fetch(requestForecastWeatherURL, {
+        method: 'GET' //GET is the default.
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data2) {
+        console.log(data2);
+        localStorage.setItem(city+'2',JSON.stringify(data2));
+    });
+    
 });
